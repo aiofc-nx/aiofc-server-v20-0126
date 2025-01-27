f88458ab6f93e080883dacdacee17a7923b3d867
arligle <0774543002@163.com>
2025-01-28 03:22:05 +0800

    fix(monorepo): .gitignore

--- dist/out-tsc/apps/platform/src/config/config.service.d.ts
+++
@@ -1,3 +0,0 @@
-import { AppConfig } from './options/app.config';
-import { DatabaseConfig } from './options/database.config';
-export declare const ConfigService: (typeof AppConfig | typeof DatabaseConfig)[];
--- dist/out-tsc/packages/zod-env/src/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/z-yaml-loader';
-export * from './lib/zod-env';
